cbf8885923585aff9e8e411223a09a4ce09aad1d
MythikBsdev <brice.torren04@gmail.com>
2025-10-16 21:58:10 +0100

    login fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,1 @@
-import { redirect } from "next/navigation";
-
-import { createSupabaseServerClient } from "@/lib/supabase/server";
-
-const HomePage = async () => {
-  const supabase = await createSupabaseServerClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (user) {
-    redirect("/dashboard");
-  }
-
-  redirect("/login");
-};
-
-export default HomePage;
+﻿import { redirect } from "next/navigation";import { createSupabaseServerClient } from "@/lib/supabase/server";const HomePage = async () => {  const supabase = await createSupabaseServerClient();  const {    data: { user },  } = await supabase.auth.getUser();  if (user) {    redirect("/invoices");  }  redirect("/login");};export default HomePage;
