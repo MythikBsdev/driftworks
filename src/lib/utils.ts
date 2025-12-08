30d17d24734adfaafb83629d706e954172dafc15
MythikBsdev <brice.torren04@gmail.com>
2025-12-08 19:29:52 +0000

    $

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,12 +3,14 @@ import { twMerge } from "tailwind-merge";
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
-export const currencyFormatter = (currency: string, locale = "en-GB") =>
-  new Intl.NumberFormat(locale, {
+export const currencyFormatter = (currency: string, locale?: string) => {
+  const resolvedLocale = locale ?? (currency === "USD" ? "en-US" : "en-GB");
+  return new Intl.NumberFormat(resolvedLocale, {
     style: "currency",
     currency,
     minimumFractionDigits: 2,
   });
+};
 
 export const sum = (values: number[]) => values.reduce((acc, value) => acc + value, 0);
 
